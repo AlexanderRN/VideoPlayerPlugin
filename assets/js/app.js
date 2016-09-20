9df4c12450bb3d1b23c441b182f3afc46d35de1f
Alexander R Nielsen <alexanderrnielsen@hotmail.dk>
2016-09-20 12:29:42 +0300

    WIP Plugin with ability to add video players

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,44 @@
-/**
- * Created by alexandernielsen on 20/09/16.
- */
+(function($) {
+
+  var VideoPlayer = function(element, options) {
+
+    var elem = $(element);
+    var obj = this;
+
+    var settings = $.extend({
+      param: 'defaultValue'
+    }, options || {});
+
+    // Public method - can be called from client code
+    this.changeContent = function(value) {
+      elem.html(value);
+    };
+
+    this.openVideo = function(videoPath) {
+      var video = '<video class="embed-responsive-item" width="520" height="360" controls>' +
+          '<source src="" type="video/mp4">' +
+          '</video>';
+
+      elem.html(video);
+      elem.find('video').attr("src", videoPath);
+
+      // Causes error.
+     // elem.find('video').load();
+    }
+  };
+
+  $.fn.videoPlayer = function(options) {
+    var element = $(this);
+
+    // Return early if this element already has a plugin instance
+    if (element.data('videoplayer')) return element.data('videoplayer');
+
+    // pass options to plugin constructor
+    var videoplayer = new VideoPlayer(this, options);
+
+    // Store plugin object in this element's data
+    element.data('videoplayer', videoplayer);
+
+    return videoplayer;
+  };
+})(jQuery);
